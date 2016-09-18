9b0790c30d0acaa22f1484b657a7d234623440a4
gjones.pf@gmail.com <gjones.pf@gmail.com>
2016-09-19 00:12:36 +1200

    Tweaks to get syntax correct

--- scripts/mc-mirror-src2dest.sh
+++ scripts/mc-mirror-src2dest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$UPLOAD_SOURCE" ]; then
+if [ ! "$UPLOAD_SOURCE" ]; then
    mc config host add "s3-src" "$UPLOAD_SOURCE" "$UPLOAD_SOURCE_KEY" "$UPLOAD_SOURCE_SECRET" S3v4 
    echo "Source: $UPLOAD_SOURCE/$UPLOAD_SOURCE_BUCKET - $UPLOAD_SOURCE_KEY"
    mc ls s3-src
@@ -8,9 +8,8 @@ if [ "$UPLOAD_SOURCE" ]; then
    SOURCE=s3-src/$UPLOAD_SOURCE_BUCKET
 fi
 
-if [ "$UPLOAD_DEST" ]; then
+if [ ! "$UPLOAD_DEST" ]; then
    mc config host add "s3-dest" "$UPLOAD_DEST" "$UPLOAD_DEST_KEY" "$UPLOAD_DEST_SECRET" S3v4 
-
    echo "Dest: $UPLOAD_DEST/$UPLOAD_DEST_BUCKET - $UPLOAD_DEST_KEY"
    mc ls s3-dest
 
@@ -18,14 +17,17 @@ if [ "$UPLOAD_DEST" ]; then
    mc mb $DEST
 fi
 
-if [ "$UPLOAD_SOURCE_FOLDER" ]; then
-   SOURCE=UPLOAD_SOURCE_FOLDER
+if [ ! "$UPLOAD_SOURCE_FOLDER" ]; then
+   echo "Using source folder: $UPLOAD_SOURCE_FOLDER"
+   SOURCE=$UPLOAD_SOURCE_FOLDER
 fi
 
-if [ "$UPLOAD_DEST_FOLDER" ]; then
-   DEST=UPLOAD_DEST_FOLDER
+if [ ! "$UPLOAD_DEST_FOLDER" ]; then
+   echo "Using dest folder: $UPLOAD_DEST_FOLDER"
+   DEST=$UPLOAD_DEST_FOLDER
 fi
 
 #TODO: Check source and dest...
-mc $UPLOAD_OPTIONS mirror $SOURCE $DEST --force
+echo "Using: mc mirror $UPLOAD_OPTIONS $SOURCE $DEST"
+mc mirror $UPLOAD_OPTIONS $SOURCE $DEST
 
